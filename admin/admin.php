0be5766ecd7fa6598d60e8a2a905afdcc2489b10
Ben <benjamincmarks@gmail.com>
2016-01-31 22:58:12 -0500

    QoL improvements for data entry, some refactoring
    Linked tables now dynamically pull data into a select menu

--- admin/admin.php
+++ admin/admin.php
@@ -1,8 +1,18 @@
 <?php
 
 global $capability;
-
 $capability = 'edit_pages';
+
+global $rep_table;
+global $bill_table;
+global $vote_table;
+global $cat_table;
+global $val_table;
+$rep_table = $wpdb->prefix . "sa_watch_representative"; //TODO: Refactor these with sa_watch.php
+$bill_table = $wpdb->prefix . "sa_watch_bill";
+$vote_table = $wpdb->prefix . "sa_watch_vote_id";
+$cat_table = $wpdb->prefix . "sa_watch_budget_item";
+
 
 add_action( 'admin_menu', 'sawatch_admin_creation' );
 
@@ -17,6 +27,7 @@ function sawatch_admin_creation() {
 
 function sa_process_rep() {
 	global $wpdb;
+	global $rep_table;
 
 	//Validate data
 	if (empty($_POST["firstname"]) || empty($_POST["lastname"]) || 
@@ -34,8 +45,6 @@ function sa_process_rep() {
 		echo "The picture is not a valid URL";
 		return;
 	}
-
-	$rep_table = $wpdb->prefix . "sa_watch_representative"; //TODO: Refactor this with sa_watch.php
 	//Search the database for this userID
 	$results = $wpdb->get_results( "SELECT rep_id FROM " . $rep_table . " WHERE firstname='" . $_POST["firstname"] . 
 									"' AND lastname='" . $_POST["lastname"] ."';", OBJECT);
@@ -60,33 +69,29 @@ function sa_process_rep() {
 
 function sa_process_bill() {
 	global $wpdb;
-	$bill_table = $wpdb->prefix . "sa_watch_bill"; //TODO: Refactor this with sa_watch.php
+	global $bill_table;
 	//I am a stub
 }
 
 function sa_process_vote() {
 	global $wpdb;
-	$vote_table = $wpdb->prefix . "sa_watch_vote_id"; //TODO: Refactor this with sa_watch.php
+	global $vote_table;
 	//I am a stub
 }
 
 function sa_process_cat() {
 	global $wpdb;
-	$cat_table = $wpdb->prefix . "sa_watch_budget_item"; //TODO: Refactor this with sa_watch.php
+	global $cat_table;
 	//I am a stub
 }
 
 function sa_process_val() {
 	global $wpdb;
-	$val_table = $wpdb->prefix . "sa_watch_budget_value"; //TODO: Refactor this with sa_watch.php
+	global $val_table;
 	//I am a stub
 }
 
 function sa_process_form() {
-	//Check if this is a form submission
-	if (!isset($_POST["type"])) {
-		return;
-	}
 	switch($_POST["type"]) {
 		case "rep":
 			sa_process_rep();
@@ -107,13 +112,23 @@ function sa_process_form() {
 			//This should never happen
 			echo "ERROR: Illegal submission type";
 	}
+	return $_POST["type"];
 }
 
 
 //Creates the HTML Admin Pane
 function sa_data_entry() {
-	sa_process_form();
-	//TODO: Reload same section the form was submitted from
+	global $wpdb;
+	global $rep_table;
+	global $bill_table;
+	global $cat_table;
+	//Check if this is a form submission
+	if (isset($_POST["type"])) {
+		$type = sa_process_form();
+	} else {
+		$type = "rep"; //Default to representative screen
+	}
+	//TODO: Display Budget Names/Rep Names via dropdown menu
 ?>
 	<div class="sawatch-admin-pane">
 		<h1> SA Data Entry </h1>
@@ -126,15 +141,15 @@ function sa_data_entry() {
 				<li id="val" onclick="updatePane(this.id)">Budget Values</li>
 			</ul>
 		</nav>
-		<div id="rep-data">
-			<p>Representative Input</p>
+		<div id="rep-data" <?php if ($type != "rep") echo 'style="display:none;"'; ?>>
 			<form action="" method="post">
+				<h3>Representative Input</h3>
 				First name:<br>
 				<input type="text" name="firstname"><br>
 				Last name:<br>
 				<input type="text" name="lastname"><br>
 				Class Year:<br>
-				<input type="text" name="classyear"><br>
+				<input type="number" name="classyear"><br>
 				Position:<br>
 				<input type="radio" name="position" value="pres" checked>President<br>
 				<input type="radio" name="position" value="vp">Vice President<br>
@@ -147,8 +162,8 @@ function sa_data_entry() {
 				<input type="hidden" name="type" value="rep"> <!-- Used when processing form -->
 			</form>
 		</div>
-		<div id="bill-data" style="display:none;">
-			<p>Bill Input</p>
+		<div id="bill-data" <?php if ($type != "bill") echo 'style="display:none;"'; ?>>
+			<h3>Bill Input</h3>
 			<form action="" method="post">
 				Bill Name:<br>
 				<input type="text" name="name"><br>
@@ -164,15 +179,27 @@ function sa_data_entry() {
 				<input type="hidden" name="type" value="bill"> <!-- Used when processing form -->
 			</form>
 		</div>
-		<div id="vote-data" style="display:none;">
-			<p>Votes Input</p>
+		<div id="vote-data" <?php if ($type != "vote") echo 'style="display:none;"'; ?>>
+			<h3>Votes Input</h3>
 			<form action="" method="post">
-				Representative First Name:<br>
-				<input type="text" name="firstname"><br>
-				Representative Last Name:<br>
-				<input type="text" name="lastname"><br>
+				Representative:<br>
+				<?php 
+				$results = $wpdb->get_results( "SELECT rep_id, firstname, lastname FROM " . $rep_table  .";", OBJECT);
+				echo '<select name="rep">';
+				foreach ($results as $rep) {
+					echo "<option value=" . $rep->rep_id . "> " . $rep->firstname . " " . $rep->lastname . "</option>";
+				}
+				echo "</select><br>";
+				?>
 				Bill Name:<br>
-				<textarea type="text" name="bill_name"></textarea><br>
+				<?php 
+				$results = $wpdb->get_results( "SELECT bill_id, name FROM " . $bill_table  .";", OBJECT);
+				echo '<select name="bill">';
+				foreach ($results as $bill) {
+					echo "<option value=" . $bill->bill_id . "> " . $bill->name . "</option>";
+				}
+				echo "</select><br>";
+				?>
 				Result:<br>
 				<input type="radio" name="vote" value="aye" checked>Aye<br>
 				<input type="radio" name="vote" value="nay">Nay<br>
@@ -181,8 +208,8 @@ function sa_data_entry() {
 				<input type="hidden" name="type" value="vote"> <!-- Used when processing form -->
 			</form>
 		</div>
-		<div id="cat-data" style="display:none;">
-			<p>Budget Category Input</p>
+		<div id="cat-data" <?php if ($type != "cat") echo 'style="display:none;"'; ?>>
+			<h3>Budget Category Input</h3>
 			<form action="" method="post">
 				Budget Category Name:<br>
 				<input type="text" name="name"><br>
@@ -192,15 +219,22 @@ function sa_data_entry() {
 				<input type="hidden" name="type" value="cat"> <!-- Used when processing form -->
 			</form>
 		</div>
-		<div id="val-data" style="display:none;">
-			<p>Budget Value Input</p>
+		<div id="val-data" <?php if ($type != "val") echo 'style="display:none;"'; ?>>
+			<h3>Budget Value Input</h3>
 			<form action="" method="post">
 				Budget Category Name:<br>
-				<input type="text" name="cat_name"><br>
+				<?php 
+				$results = $wpdb->get_results( "SELECT budget_id, name FROM " . $cat_table  .";", OBJECT);
+				echo '<select name="cat">';
+				foreach ($results as $cat) {
+					echo "<option value=" . $cat->budget_id . "> " . $cat->name . "</option>";
+				}
+				echo "</select><br>";
+				?>
 				Date:<br>
 				<input type="date" name="date"><br>
 				Amount:<br>
-				<input type="text" name="amount"><br>
+				<input type="number" step="0.01" name="amount"><br>
 				<input type="submit" value="Submit">
 				<input type="hidden" name="type" value="val"> <!-- Used when processing form -->
 			</form>
